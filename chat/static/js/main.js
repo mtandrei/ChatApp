af9964a40c9b13bf8f7f4502655ff8b3b1e316be
Srikar Gudipati <srikarvgudipati@gmail.com>
2016-10-07 03:27:44 -0400

    Listing messages works

--- chat/static/js/main.js
+++ chat/static/js/main.js
@@ -1,3 +1,18 @@
 $(function() {
-    
+    var messages = $('.messages');
+    $.ajax({
+        url: 'http://localhost:5000/messages',
+        dataType: 'json',
+    })
+    .done(function(data) {
+        for (var messageData in data) {
+            if (!data.hasOwnProperty(messageData)) continue;
+
+            var obj = data[messageData];
+            for (var prop in obj) {
+                if (!obj.hasOwnProperty(prop)) continue;
+                messages.append('<li><strong>' + prop + '</strong> said ' + obj[prop] + '</li>');
+            }
+        }
+    });
 });
